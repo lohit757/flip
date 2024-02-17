e1ce609720d313d9b490db61b4fe65c4f92bdbbc
lohit757 <106148604+lohit757@users.noreply.github.com>
2024-02-17 23:32:13 +0530

    Add files via upload

---
+++ flip.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>flipping cards</title>
+    <link rel="stylesheet" href="flip.css">
+</head>
+<body>
+    <header class="header">
+        <a href="#" class="logo">Learning<br><b>LABS</b></a>
+        <nav class="navbar">
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Gallery</a>
+            <a href="">Services</a>
+            <a href="">Contact</a>
+        </nav>
+    </header>
+    <div class="card">
+        <div class="face front">
+            <img src="./city.jpg.crdownload" alt="">
+            <h3>London</h3>
+        </div>
+        <div class="face back">
+            <h3>London</h3>
+            <p>london is one of my favourite place.
+                <div class="link">
+                    <a href="#">Details</a>
+                    
+                </div>
+            </p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="face front">
+            <img src="lake.jpg.crdownload" alt="">
+            <h3>Lake</h3>
+        </div>
+        <div class="face back">
+            <h3>Lake</h3>
+            <p>I like to swim in lake.
+                <div class="link">
+                    <a href="#">Details</a>
+                    
+                </div>
+            </p>
+        </div>
+    </div>
+    <div class="card">
+        <div class="face front">
+            <img src="lone.jpg.crdownload" alt="">
+            <h3>Lonely tree</h3>
+        </div>
+        <div class="face back">
+            <h3>Lonely tree</h3>
+            <p>i like to be alone like this.
+                <div class="link">
+                    <a href="#">Details</a>
+                    
+                </div>
+            </p>
+        </div>
+    </div>
+</body>
+</html>
